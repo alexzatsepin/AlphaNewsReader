8cbe9f6a554f81981632c1c305423991cb0b8bf4
a.zacepin <a.zacepin@corp.mail.ru>
2014-08-14 23:07:02 +0400

    initial commit

---
+++ app/src/main/java/alphanews/newsapp/MyIntentService.java
@@ -0,0 +1,26 @@
+package alphanews.newsapp;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ */
+public class Rss extends IntentService {
+
+    public Rss() {
+        super("MyIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            }
+        }
+    }
+
+}
